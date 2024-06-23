cc98ef1ebfe84508b1b8000e5aebc349ae97e2f0
prithvi sharma <prithvi2255@gmail.com>
2024-06-23 23:03:29 +0530

    update(service): implement get endpoints service logics, implement player two join game logic

--- src/service/game-service.ts
+++ src/service/game-service.ts
@@ -7,7 +7,19 @@ const getAllGames = async (req: CustomRequest, res: Response) => {
   const games = await database.games?.find().toArray()
   return res
     .status(200)
-    .json({ ok: true, message: 'Users Found', data: { games } })
+    .json({ ok: true, message: 'Games Found', data: { games } })
+}
+
+const getGame = async (req: CustomRequest, res: Response) => {
+  const gameId = req.params.gameId
+  const game: Game | null | undefined = await database.games?.findOne<Game>({
+    gameId: gameId,
+  })
+  if (!game)
+    return res.status(404).json({ ok: false, message: 'Game Not Found' })
+  return res
+    .status(200)
+    .json({ ok: true, message: 'Game Found', data: { game } })
 }
 
 const joinGame = async (req: CustomRequest, res: Response) => {
@@ -26,18 +38,19 @@ const joinGame = async (req: CustomRequest, res: Response) => {
         .json({ ok: false, message: 'Internal Server Error' })
     }
   } else {
-    const dbGame: Game | null | undefined = await database.games?.findOne<Game>(
-      {
-        gameId: gameId,
-      }
+    const updateResult = await database.games?.updateOne(
+      { gameId: gameId },
+      { $set: { 'players.playerTwoId': userId } }
     )
-    if (!dbGame) {
-      return res.status(404).json({ ok: false, message: 'Game Not Found' })
-    }
-    console.log(dbGame)
-  }
 
-  return res.json({ ok: true, message: 'Game Joined' })
+    if (updateResult?.modifiedCount === 1) {
+      return res.status(200).json({ ok: true, message: 'Game Joined' })
+    } else {
+      return res
+        .status(500)
+        .json({ ok: false, message: 'Failed to update game' })
+    }
+  }
 }
 
 const playerAttack = async (req: CustomRequest, res: Response) => {
@@ -48,4 +61,4 @@ const playerDefend = async (req: CustomRequest, res: Response) => {
   return res.json({ ok: true, message: 'Player Defended' })
 }
 
-export { getAllGames, joinGame, playerAttack, playerDefend }
+export { getAllGames, getGame, joinGame, playerAttack, playerDefend }
