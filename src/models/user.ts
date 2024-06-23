79821f28b9c6f2241f20efc6a840ca0acc802ccc
prithvi sharma <prithvi2255@gmail.com>
2024-06-23 20:36:37 +0530

    update(user): User interface add properties userId, password

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,19 +1,25 @@
 interface User {
   id?: string
   name: string
+  userId: string
+  password?: string
   defenseSetLength: number
   role: string
 }
 
 function initUser(userData: {
   name: string
+  userId: string
+  password: string
   defenseSetLength: number
   role: string
 }): User {
-  const { name, defenseSetLength, role } = userData
+  const { name, defenseSetLength, role, userId, password } = userData
   return {
     id: crypto.randomUUID(),
     name: name,
+    userId: userId,
+    password: password,
     defenseSetLength: defenseSetLength,
     role: role,
   }
