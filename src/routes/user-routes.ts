6084db658aefc9e6a4e47eb49d6f9cfe4ba0ca56
prithvi sharma <prithvi2255@gmail.com>
2024-06-23 20:39:13 +0530

    update(routes): api path modifications add get all users endpoint

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express'
-import { createUser, editUser, getUser } from '../service/user-service'
+import {
+  createUser,
+  editUser,
+  getUser,
+  getUsers,
+} from '../service/user-service'
 
 const userRoutes = Router()
 
 userRoutes.post('/create', createUser)
 
-userRoutes.put('/edit', editUser)
+userRoutes.put('/edit/:userId', editUser)
 
 userRoutes.get('/get/:userId', getUser)
+
+userRoutes.get('/get', getUsers)
 
 export { userRoutes }
