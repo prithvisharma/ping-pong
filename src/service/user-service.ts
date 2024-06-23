9139702f2ae7d84cf63625ca39c4791ac7ce75ec
prithvi sharma <prithvi2255@gmail.com>
2024-06-23 20:39:52 +0530

    update(service): implement logic for each service method

--- src/service/user-service.ts
+++ src/service/user-service.ts
@@ -1,16 +1,51 @@
+import bcrypt from 'bcrypt'
 import { Request, Response } from 'express'
+import { database } from '../config/mongo'
 import { CustomRequest } from '../models/custom-request'
+import { initUser, User } from '../models/user'
 
 const createUser = async (req: Request, res: Response) => {
-  return res.json({ ok: true, message: 'Game Joined' })
+  const { name, userId, password, defenseSetLength, role } = req.body
+  if (!name || !userId || !password || !defenseSetLength || !role) {
+    return res
+      .status(400)
+      .json({ ok: false, message: 'Invalid Request Body Data' })
+  }
+  const encryptedPassword = await bcrypt.hash(password, 10)
+  const user: User = initUser({
+    name,
+    userId,
+    password: encryptedPassword,
+    defenseSetLength,
+    role,
+  })
+  const dbResult = await database.users?.insertOne(user)
+  if (dbResult?.acknowledged && dbResult?.insertedId) {
+    return res.status(200).json({ ok: true, message: 'User Created' })
+  } else {
+    return res.status(500).json({ ok: false, message: 'Internal Server Error' })
+  }
 }
 
 const editUser = async (req: CustomRequest, res: Response) => {
-  return res.json({ ok: true, message: 'Player attacked' })
+  return res.status(503).json({ ok: false, message: 'Endpoint Unavailable' })
 }
 
 const getUser = async (req: CustomRequest, res: Response) => {
-  return res.json({ ok: true, message: 'Player Defended' })
+  const { userId } = req.params
+  const user = await database.users?.findOne({ userId: userId })
+  if (!user)
+    return res.status(404).json({ ok: false, message: 'User Not Found' })
+  return res
+    .status(200)
+    .json({ ok: true, message: 'User Found', data: { user } })
 }
 
-export { createUser, editUser, getUser }
+const getUsers = async (req: CustomRequest, res: Response) => {
+  const users = await database.users?.find().toArray()
+  return res
+    .status(200)
+    .json({ ok: true, message: 'Users Found', data: { users } })
+}
+
+export { createUser, editUser, getUser, getUsers }
