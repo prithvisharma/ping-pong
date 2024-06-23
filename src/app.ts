606d6d811972b760be901ab0b17e3ee01b067f87
prithvi sharma <prithvi2255@gmail.com>
2024-06-23 21:36:47 +0530

    add(): dotenv package initialise dotenc.config()

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import express from 'express'
 import { connectToDatabase } from './config/mongo'
 import { registerRoutes } from './register-routers'
 
-const port = process.env.PORT ?? 3000
+const port = process.env.PORT
 
 const app = express()
 
