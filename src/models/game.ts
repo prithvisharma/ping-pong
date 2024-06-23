31ac88386700eeb5f4eb503d899b12d584ad32cd
prithvi sharma <prithvi2255@gmail.com>
2024-06-23 18:43:21 +0530

    update(model): rework Game model

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,9 +1,33 @@
-import { Player } from './player'
+import { User } from './user'
 
-export interface Game {
-  _id?: string
-  player1: Player['_id']
-  player2: Player['_id']
-  score1: number
-  score2: number
+interface Game {
+  id?: string
+  players: {
+    playerOneId: User['id']
+    playerTwoId: User['id']
+    playerOneScore: number
+    playerTwoScore: number
+  }
+  moves?: { playerId: User['id']; moveType: string }[]
+  winner?: User['id']
 }
+
+function initGame(playersData: {
+  playerOneId: User['id']
+  playerTwoId: User['id']
+}): Game {
+  const { playerOneId, playerTwoId } = playersData
+  return {
+    id: crypto.randomUUID(),
+    players: {
+      playerOneId: playerOneId,
+      playerTwoId: playerTwoId,
+      playerOneScore: 0,
+      playerTwoScore: 0,
+    },
+    moves: [],
+    winner: undefined,
+  }
+}
+
+export { Game, initGame }
